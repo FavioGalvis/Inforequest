0b9abee3af201db93200e3bde1398ed3791c8fb2
Favio Galvis <favio.agg@gmail.com>
2015-05-21 12:01:37 -0500

    Enterprise change to layout of footer function.

--- core/layout_api.php
+++ core/layout_api.php
@@ -975,14 +975,14 @@ function layout_footer() {
 
 	# Show MantisBT version and copyright statement
 	$t_version_suffix = '';
-	$t_copyright_years = ' 2000 - ' . date( 'Y' );
+	$t_copyright_years = ' 2015 - ' . date( 'Y' );
 	if( config_get( 'show_version' ) == ON ) {
 		$t_version_suffix = ' ' . htmlentities( MANTIS_VERSION . config_get_global( 'version_suffix' ) );
 	}
 	echo '<div class="col-md-6 col-xs-12 no-padding">' . "\n";
 	echo '<address>' . "\n";
-	echo '<strong>Powered by <a href="http://www.mantisbt.org" title="bug tracking software">MantisBT ' . $t_version_suffix . '</a></strong> <br>' . "\n";
-	echo "<small>Copyright &copy;$t_copyright_years MantisBT Team</small>" . '<br>';
+	echo '<strong>Desarrollado por <a href="http://www.infortributos.com" title="Software de Requerimientos">Informatica y Tributos S.A.S. ' . $t_version_suffix . '</a></strong> <br>' . "\n";
+	echo "<small>Copyright &copy;$t_copyright_years Inforequest</small>" . '<br>';
 
 	# Show optional user-specified custom copyright statement
 	$t_copyright_statement = config_get( 'copyright_statement' );
@@ -1005,11 +1005,11 @@ function layout_footer() {
 	if( !is_page_name( 'login_page' ) ) {
 		echo '<div class="col-md-6 col-xs-12">' . "\n";
 		echo '<div class="pull-right" id="powered-by-mantisbt-logo">' . "\n";
-		$t_mantisbt_logo_url = helper_mantis_url( 'images/mantis_logo.png' );
-		echo '<a href="http://www.mantisbt.org" '.
-			'title="Mantis Bug Tracker: a free and open source web based bug tracking system.">' .
+		$t_mantisbt_logo_url = helper_mantis_url( 'images/infortributos_logo.png' );
+		echo '<a href="http://www.infortributos.com" '.
+			'title="Desarrollado por Informatica y Tributos S.A.S.">' .
 			'<img src="' . $t_mantisbt_logo_url . '" width="102" height="35" ' .
-			'alt="Powered by Mantis Bug Tracker: a free and open source web based bug tracking system." />' .
+			'alt="Desarrollado por Informatica y Tributos S.A.S." />' .
 			'</a>' . "\n";
 		echo '</div>' . "\n";
 		echo '</div>' . "\n";
